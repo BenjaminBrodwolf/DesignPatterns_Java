a13665dcd166be95785903cc5de58a44364eefc5
Turan Ledermann <turan.ledermann@students.fhnw.ch>
2020-01-26 17:40:43 +0100

    add comment

--- src/factory_method/v3_factory_method/Garage.java
+++ src/factory_method/v3_factory_method/Garage.java
@@ -2,9 +2,12 @@ package factory_method.v3_factory_method;
 
 //CREATOR
 public abstract class Garage {
+    
     public Car getCar(String type) {
         Car car = createCar(type);
         return car;
     }
+
+    //FACTORY METHOD, must be implemented in Factory classes
     protected abstract Car createCar(String type);
 }
